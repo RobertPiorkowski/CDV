7429b09078481ba01d40c9334e876f9e93d83f3f
Robert Piórkowski <rpiorkowski@edu.cdv.pl>
2020-03-04 15:28:14 +0100

    Second file

--- Programowanie strukturalne/podstawy.py
+++ Programowanie strukturalne/podstawy.py
@@ -1,1 +1,39 @@
 print("CDV")
+print(10)
+'''
+komentarz
+blokowy
+'''
+#Potęgowanie
+
+potega=2 ** 10
+print(potega)
+
+#Pobieranie danych z klawiatury
+
+name=input()
+# + konkatenacja
+print("Twoje imię: " + name)
+surname=input()
+print("Twoje imie: " +name+", Nazwisko: " +surname)
+
+'''
+Uzytkownik podaje z klawiatury swój wiek
+Wyświetl dane w formacie: Twój wiek: ... lat
+'''
+
+print("Podaj swój wiek: ",end="")
+age=input()
+print(type(age))
+print("Twój wiek: ", age, " lat")
+age1=20
+print(type(age1))
+
+surname="Kowalski"
+firstletter=surname[0]
+print(firstletter)
+length=len(surname)
+print(length)
+
+lastLetter=surname[-1]
+print(lastLetter)
